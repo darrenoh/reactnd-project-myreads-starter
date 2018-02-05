e7b83d6f39ab292bde393fbe038e4776047d6946
Darren Oh <darren@oh.name>
2018-02-05 17:26:04 -0500

    feat: Use API to populate book shelves

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
-  state = {}
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books })
+    })
+  }
 
   render() {
+    const { books } = this.state
+
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBooks/>
+          <ListBooks
+            books={books}
+          />
         )}/>
         <Route path="/search" render={() => (
           <SearchBooks/>
