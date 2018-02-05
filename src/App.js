f0361927b680ae78c4fb97cc2ddb7a9fbad8e910
Darren Oh <darren@oh.name>
2018-02-05 15:33:31 -0500

    feat: Replace state variable with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+  state = {}
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks/>
-        ) : (
+        <Route exact path="/" render={() => (
           <ListBooks/>
-        )}
+        )}/>
+        <Route path="/search" render={() => (
+          <SearchBooks/>
+        )}/>
       </div>
     )
   }
